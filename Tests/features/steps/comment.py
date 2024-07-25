21ba03045fc1f2199117073e8f50657e06511f0a
MeliLiz <melifernandez@ciencias.unam.mx>
2024-07-25 14:25:30 -0600

    Getting to the product

--- Tests/features/steps/comment.py
+++ Tests/features/steps/comment.py
@@ -44,6 +44,7 @@ def step_impl(context):
     
 @when('Comment the product')
 def step_impl(context):
+    context.product_commented = context.driver.find_element(By.XPATH, "/html/body/div/div[1]/main/section/div/div/div[2]/h1").text
     # Star the product
     stars = context.driver.find_elements(By.CLASS_NAME, "star")
     last_star = stars[-1] 
@@ -51,14 +52,28 @@ def step_impl(context):
     actions.move_to_element(last_star).click().perform()
     # Fill the comment field
     comment = context.driver.find_element(By.XPATH, "/html/body/div/div[1]/main/section/div/div/div[2]/div[2]/textarea")
-    comment.send_keys("Excelente producto")
+    comment.send_keys("Muy ricas")
     # Click on the comment button
     context.driver.find_element(By.XPATH, "/html/body/div/div[1]/main/section/div/div/div[2]/button").click()
     
 @then('Check if the product is commented')
 def step_impl(context):
-    wait = WebDriverWait(context.driver, 10)
+    # Go to see the products
+    products_button = context.driver.find_element(By.XPATH, "/html/body/div/div[1]/nav/div/div/ul/li[2]/a")
+    products_button.click()
+    product = context.driver.find_element(By.XPATH ,f"//h3[text() = '{context.product_commented}']")
+    if product:
+        try: # Click on the last row
+            context.driver.execute_script("arguments[0].click();", product)
+            print("The product was clicked successfully.")
+        except Exception as e:
+            print(f"Failed to click on the last row: {e}")
+        
+    else:
+        print("The product has been sold out")
+        pass
+    """wait = WebDriverWait(context.driver, 10)
     time.sleep(5)
     # Check if there is an h5 with the text "Excelente producto"
     h5 = context.driver.find_element(By.XPATH, "//h5[text()='Excelente producto']")
-    assert h5
+    assert h5"""
