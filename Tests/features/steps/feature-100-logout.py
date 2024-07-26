3702a4b5e5a9e5a1c8498371bfb7748d7261eb27
IsaacAE <alcantara_estrada_kevin@ciencias.unam.mx>
2024-07-25 23:02:26 -0600

    Se crea el feature para hacer logout

---
+++ Tests/features/steps/feature-100-logout.py
@@ -0,0 +1,38 @@
+from behave import given, when, then
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+"""Feature: Add a comment
+    Scenario: Comment a product
+    Given We are in the website
+    When Go to my shopping
+    When Select the last element bought
+    when Comment the product
+    Then Check if the product is commented"""
+    
+@given('estando en el sitio web')
+def step_impl(context):
+    context.driver.get(context.config.get('URL', 'link'))
+    
+@when('estamos en la pagina principal')
+def step_impl(context):
+    pass
+    
+@then('cerrar sesion de perfil')
+def step_impl(context):
+    options =  context.driver.find_element(By.CSS_SELECTOR, 'a.nav-link.dropdown-toggle')
+    options.click()
+   
+    
+
+    # Encontrar y hacer clic en la tercera opción del menú desplegable
+    third_option = context.driver.find_element(By.XPATH, '/html/body/div/div[1]/nav/div/div/div/div/div/a[3]')
+    third_option.click()
+    
+
+    
+
